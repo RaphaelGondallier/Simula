bf6f74a5cdb15cdb37a7005c53d6fe34f1e29fbc
RaphaelGondallier <165203397+RaphaelGondallier@users.noreply.github.com>
2024-07-10 16:42:43 +0200

    Update Calculate_AR.py

--- Aspect_ratio/Calculate_AR.py
+++ Aspect_ratio/Calculate_AR.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 16:39:14 2024
+
+@author: Raphaël Gondallier de Tugny
+"""
+
 # trace generated using paraview version 5.12.0-RC2
 #import paraview
 #paraview.compatibility.major = 5
 #paraview.compatibility.minor = 12
 
-#### import the simple module from the paraview
 from paraview.simple import *
 import pandas as pd
 from math import sqrt
@@ -12,19 +19,25 @@ from statistics import mean
 def distance(pt1, pt2):
     return sqrt( (pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2 + (pt1[2]-pt2[2])**2 )
 
+def export_import_data(input_, csv_name):
+	clipDisplay = Show(input_, spreadSheetView1, 'SpreadSheetRepresentation')
+	ExportView(f'{path}/{csv_name}', view=spreadSheetView1, RealNumberNotation='Mixed', RealNumberPrecision=6)
+	data = pd.read_csv(rf"{path}/{csv_name}")
+	data_array = data.values
+	points = data_array[:,1:4].tolist()
+	return points
+
 path = "/Users/lisa/Documents/Simula/AspectRatio/Case9"
 
 # find source
 case9_el047_nocap_boundaries000000vtu = FindSource('case9_el047_nocap_boundaries000000.vtu')
 
-############################
-##### AVERAGE DIAMETER ####
-###########################
+# =============================================================================
+# AVERAGE DIAMETER
+# =============================================================================
 
 # create a new 'Threshold'
 threshold2 = Threshold(registrationName='Threshold2', Input=case9_el047_nocap_boundaries000000vtu)
-
-# Properties modified on threshold2
 threshold2.LowerThreshold = 33.0
 threshold2.UpperThreshold = 33.0
 UpdatePipeline(time=0.0, proxy=threshold2)
@@ -36,7 +49,7 @@ slice1.SliceType.Origin = [0.12312038894168996, 0.13432140154647867, 0.063510394
 slice1.SliceType.Normal = [0.261893954620752, -0.07346556709702588, -0.9622964028739877] #INPUT
 UpdatePipeline(time=0.0, proxy=slice1)
 
-# create a new 'Clip'
+# Restricts the zone to the aneurysm
 clip3 = Clip(registrationName='Clip3', Input=slice1)
 clip3.ClipType = 'Sphere'
 clip3.ClipType.Center = [0.12330716453601878, 0.13530589404617496, 0.06256396502637897] # INPUT
@@ -44,14 +57,9 @@ clip3.ClipType.Radius = 0.004 # INPUT
 UpdatePipeline(time=0.0, proxy=clip3)
 
 spreadSheetView1 = CreateView('SpreadSheetView')
-clip3Display = Show(clip3, spreadSheetView1, 'SpreadSheetRepresentation')
 
-# Caculate centroid and average diameter
-ExportView(f'{path}/test1.csv', view=spreadSheetView1, RealNumberNotation='Mixed',
-    RealNumberPrecision=6)
-data = pd.read_csv(rf"{path}/test1.csv")
-data_array = data.values
-border_neck_points = data_array[:,1:4].tolist()
+# Caculates centroid and average diameter
+border_neck_points = export_import_data(clip3, 'border_neck_points.csv')
 x_mean = sum([border_neck_points[i][0] for i in range(len(border_neck_points))])/len(border_neck_points)
 y_mean = sum([border_neck_points[i][1] for i in range(len(border_neck_points))])/len(border_neck_points) 
 z_mean = sum([border_neck_points[i][2] for i in range(len(border_neck_points))])/len(border_neck_points)
@@ -60,36 +68,60 @@ print("Centroid: ", centroid)
 avg_neck_diameter = 2 * mean( [distance(centroid, neck_point) for neck_point in border_neck_points] )
 print("Average neck diameter: ", avg_neck_diameter, " m")
 
-###################
-##### HEIGHT ####
-##################
+# =============================================================================
+# HEIGHT
+# =============================================================================
 
-# Slice of the whole mesh
+# Slice of the whole mesh: threshold is not applied here
 slice2 = Slice(registrationName='Slice2', Input=case9_el047_nocap_boundaries000000vtu)
 slice2.SliceType = 'Plane'
 slice2.SliceType.Origin = [0.12312038894168996, 0.13432140154647867, 0.06351039414662109] #INPUT
 slice2.SliceType.Normal = [0.261893954620752, -0.07346556709702588, -0.9622964028739877] #INPUT
 UpdatePipeline(time=0.0, proxy=slice2)
 
-clip3Display = Show(slice2, spreadSheetView1, 'SpreadSheetRepresentation')
+neck_points =  export_import_data(slice2, 'neck_points.csv')
 
-ExportView(f'{path}/neck_points.csv', view=spreadSheetView1, RealNumberNotation='Mixed',
-    RealNumberPrecision=6)
-data2 = pd.read_csv(rf"{path}/neck_points.csv")
-data_array2 = data2.values
-neck_points = data_array2[:,1:4].tolist()
+# Defines dome
+clip_dome = Clip(registrationName='Clip_dome', Input=threshold2)
+clip_dome.ClipType = 'Sphere'
+clip_dome.ClipType.Center = [0.1236790071159832, 0.13444514829080068, 0.0620668062720966] # INPUT
+clip_dome.ClipType.Radius = 0.0018298000000000006 # INPUT
+UpdatePipeline(time=0.0, proxy=clip_dome)
+
+dome_points = export_import_data(clip_dome, 'dome_points.csv')
 
 distance_max = 0
-for dome_point in dome_points:
-    xi, yi, zi = neck_point[0], neck_point[1], neck_point[1]
-    n_x, n_y, n_z = slice2.SliceType.Normal[0], slice2.SliceType.Normal[0], slice2.SliceType.Normal[0]
-    x0, y0, z0 = slice2.SliceType.Origin
-    lambda_ = (n_x*(x0-xi) + n_y*(y0-yi) + n_z*(z0-zi)) / (n_x**2 + n_y**2 + n_z**2) # parameter of the parametric equation of a 
+for i, dome_point in enumerate(dome_points):
+	xi, yi, zi = dome_point[0], dome_point[1], dome_point[2]
+	n_x, n_y, n_z = slice2.SliceType.Normal[0], slice2.SliceType.Normal[1], slice2.SliceType.Normal[2]
+	x0, y0, z0 = slice2.SliceType.Origin
+	lambda_ = (n_x*(x0-xi) + n_y*(y0-yi) + n_z*(z0-zi)) / (n_x**2 + n_y**2 + n_z**2) # parameter of the parametric equation of a 
                                                                                      # line going through neck_point and perpendicular 
 																																																			# to the neck plane
-    intersection = (xi+n_x*lambda_, yi+n_y*lambda_, zi+n_z*lambda_) # intersection between the neck plane and the previous line
-    distance = distance(neck_point, intersection)
-    if distance > distance_max:
-        distance_max = distance
+	intersection = [xi+n_x*lambda_, yi+n_y*lambda_, zi+n_z*lambda_] # intersection between the neck plane and the previous line
+	distance_ = distance(dome_point, intersection)
+
+# # plots lines
+# 	line = Line(registrationName=f'Line{i}')
+# 	line.Point1 = dome_point
+# 	line.Point2 = intersection
+# 	UpdatePipeline(time=0.0, proxy=line)
+
+	if distance_ > distance_max:
+		distance_max = distance_
+		intersection_max = intersection
+		dome_point_max = dome_point
+        
+# #plots maximal height
+# line = Line(registrationName='LineMax')
+# line.Point1 = dome_point_max
+# line.Point2 = intersection_max
+# UpdatePipeline(time=0.0, proxy=line)
+# renderView1 = GetActiveViewOrCreate('RenderView')
+# lineDisplay = GetDisplayProperties(line, view=renderView1)
+# # change solid color
+# lineDisplay.AmbientColor = [0.8588235294117647, 0.0, 0.0]
+# lineDisplay.DiffuseColor = [0.8588235294117647, 0.0, 0.0]
 
 print(f"Height = {distance_max}")
+print(f"Aspect ratio = {distance_max/avg_neck_diameter}")
