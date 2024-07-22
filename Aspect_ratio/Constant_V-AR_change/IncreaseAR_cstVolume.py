c594651363e430e508945ab59c49b23e875eeb31
RaphaelGondallier <165203397+RaphaelGondallier@users.noreply.github.com>
2024-07-22 17:48:58 +0200

    Update IncreaseAR_cstVolume.py

--- Aspect_ratio/Constant_V-AR_change/IncreaseAR_cstVolume.py
+++ Aspect_ratio/Constant_V-AR_change/IncreaseAR_cstVolume.py
@@ -64,7 +64,7 @@ def set_AR(target_AR, warp_by_vector, ExtractSurface, slice1, clip1, clip_dome1,
     global AR_Scale_factor 
     # Define bounds for ScaleFactor and the desired real factor
     low = -10 # negative SF to shrink the neck
-    high = 0
+    high = 9 # In case the temporary target AR is smaller than the initial one, SF has to be positive
     #SetActiveSource(ExtractSurface)
 
     # Calculate parameters
@@ -77,7 +77,7 @@ def set_AR(target_AR, warp_by_vector, ExtractSurface, slice1, clip1, clip_dome1,
 
     i = 0
     # Bisection search loop
-    while abs(AR_ - target_AR) > epsilon and i < 500:
+    while abs(AR_ - target_AR) > epsilon and i < 500: #/!\ target_AR is not the final target here but a AR_predform from the largest dichotomy
         i += 1
         # Calculate the current ScaleFactor
         scale_factor = (low + high) / 2.0
@@ -86,13 +86,13 @@ def set_AR(target_AR, warp_by_vector, ExtractSurface, slice1, clip1, clip_dome1,
 
         # Retrieve the current volume
         AR_ = aspect_ratio(slice1, clip1, clip_dome1, spreadSheetView1)
-        #print(vol, scale_factor)
         # Adjust the bounds of the interval
         if AR_ < target_AR:
             high = scale_factor # inverted bc when scale factor decreases, AR increases
         else:
             low = scale_factor
         AR_Scale_factor = scale_factor
+        print(f"AR/bounds(small dicho): {AR_}, {low}, {high}")
     # real_scale = (abs(AR_ - AR0)) / AR0
     # error = abs(real_scale - target_scale)
     # # Print the final ScaleFactor and corresponding volume
@@ -103,6 +103,7 @@ def set_AR(target_AR, warp_by_vector, ExtractSurface, slice1, clip1, clip_dome1,
     # print(f"Iterations: {i}")
     
 def Increase_volume(V0, warp_by_vector, python_calculator):
+    global j, V_Scale_factor
     # Go to the last iteration of the displacement simulation
     animationScene1 = GetAnimationScene()
     animationScene1.GoToLast()
@@ -111,7 +112,7 @@ def Increase_volume(V0, warp_by_vector, python_calculator):
     #SetActiveSource(python_calculator)
     
     # Define bounds for ScaleFactor and the desired real factor
-    low = 0
+    low = -9 # if the AR temporarly gets smaller than the initial one because of the iterative procces, SF has to be negative
     high = 10    
     # Calculate parameters
     warp_by_vector.ScaleFactor = 0
@@ -138,6 +139,8 @@ def Increase_volume(V0, warp_by_vector, python_calculator):
             low = scale_factor
         else:
             high = scale_factor
+        V_Scale_factor = scale_factor
+    print(f"Volume at iteration {j}: {vol}\n")
     
 ###########################################################  
 # Go to the last iteration of the displacement simulation
@@ -173,18 +176,23 @@ python_calculator2 = FindSource("PythonCalculator2")
 # MAIN
 # =============================================================================
 warp_by_vector1.ScaleFactor = 0
+warp_by_vector2.ScaleFactor = 0
 AR0 = aspect_ratio(slice1, clip1, clip_dome1, spreadSheetView1)
 target_scale = 0.22
 target_AR = AR0 * target_scale + AR0
+print(f"AR0 and target AR: {AR0}, {target_AR}")
 epsilon = 1e-5 * target_AR
 j=0
-real_AR=AR0
-iter_max = 2
+AR_final=AR0
+iter_max = 25
 AR_inf = 0 
-AR_sup = 10
+AR_sup = 2.571
 V0 = get_volume(python_calculator1)
+print(f"Initial volume: {V0}")
 AR_Scale_factor = 0 # global variable
-while real_AR-target_AR < epsilon and j < iter_max:
+V_Scale_factor = 0 # global variable
+while abs(AR_final-target_AR) > epsilon and j < iter_max:
+    print(f"AR_inf, AR_sup: {AR_inf}, {AR_sup}")
     AR_predeform = 0.5*(AR_inf+AR_sup) # This AR is bigger than the target because it is the 
                                        # one before the volume is reset. Thus, it is not the 
                                        # one used for the shutdown test.
@@ -204,5 +212,6 @@ execution_time = end_time - start_time
 print(f"Aspect ratio goal: {target_AR}")
 print(f"Final aspect ratio: {AR_final}")
 print(f"Final AR scale factor: {AR_Scale_factor}")
+print(f"Final V scale factor: {V_Scale_factor}")
 print(f"Iterations: {j}")
 print(f"Execution time: {execution_time/60} min")
